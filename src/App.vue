635731ee0bcfc1f9893208d7317dda5c8c00cc3d
jfr <jfrsimbare@gmail.com>
2023-03-14 13:46:47 +0100

    feat:add a todolist + delete + update

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,133 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="todos">
+    <h1>Ma Todo List</h1>
+    <div>
+      <input v-model="newTodo" placeholder="Ajouter une tâche">
+      <button @click="addTodo">Ajouter</button>
     </div>
-  </header>
+    <ul class="todo-list">
+      <li v-for="(todo, index) in todos" :key="index">
+        <div :class="{ editing: todo.editing }" v-if="todo.editing">
+          <input v-model="todo.text" @keyup.enter="saveEdit(todo)" @keyup.esc="cancelEdit(todo)">
+          <button @click="saveEdit(todo, true)">Sauvegarder</button>
+          <button @click="saveEdit(todo, false)">Annuler</button>
+        </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+        <div v-else>
+          <input type="checkbox" v-model="todo.done" @change="updateStatus(todo)">
+          <span :class="{ completed: todo.done }" @click="startEdit(todo)">
+            {{ todo.text }}
+            <span v-if="todo.done"> (Terminé) </span>
+            <span v-else> (En cours) </span>
+          </span>
+          <button @click="confirmDelete(index)">Supprimer</button>
+          <button @click="editMode(todo)">Modifier</button>
+        </div>
+
+      </li>
+    </ul>
+    <div class="btn-all">
+      <button @click="deleteCompleted">Supprimer les todos terminés</button>
+      <button @click="confirmDeleteAll">Supprimer tous les todos</button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({
+          text: this.newTodo.trim(),
+          completed: false,
+          editing: false
+        })
+        this.newTodo = ''
+      }
+    },
+    updateStatus(todo) {
+      todo.completed = !todo.completed
+    },
+    startEdit(todo) {
+      todo.editing = true
+      this.$nextTick(() => {
+        this.$refs['editField'][0].focus()
+      })
+    },
+    saveEdit(todo, isNewTodo) {
+      if (todo.text.trim() === '') {
+        this.deleteTodo(this.todos.indexOf(todo))
+      } else {
+        todo.editing = false
+        if (this.todos.indexOf(todo) === -1) {
+          this.todos.push({
+            text: todo.text.trim(),
+            completed: false,
+            editing: false
+          })
+        }
+      }
+    },
+    cancelEdit(todo) {
+      todo.editing = false
+    },
+
+    confirmDelete(index) {
+      if (confirm("Êtes-vous sûr de vouloir supprimer cet élément de la liste ?")) {
+        this.deleteTodo(index)
+      }
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1)
+    },
+    editMode(todo) {
+      todo.editing = true
+      this.$nextTick(() => {
+        this.$refs['editField'][0].focus()
+      })
+    },
+    confirmDeleteAll() {
+      if (confirm("Êtes-vous sûr de vouloir supprimer tous les todos ?")) {
+        this.todos = []
+      }
+    },
+    deleteCompleted() {
+      if (confirm("Êtes-vous sûr de vouloir supprimer les todos terminés ?")) {
+        this.todos = this.todos.filter(todo => !todo.completed)
+      }
+    }
+
+  }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+</script>
+
+
+<style>
+.completed {
+  text-decoration: line-through;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.todos {
+  margin: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.todo-list {
+  list-style: none;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.todo-list>li {
+  margin-bottom: 10px;
+}
+.btn-all {
+  margin-top: 15px;
+  margin-bottom: 15px;
 }
 </style>
